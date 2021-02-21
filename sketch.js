4a20675f62b3e9791afa80ed9c6a2b14161c2962
MimansaHe <76689108+MimansaHe@users.noreply.github.com>
2021-02-21 16:57:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -104,13 +104,13 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     console.log(hour);
-    if(hour<=0600 && hour>=1900){
+    if(hour>=0600 && hour<=1900){
         bg = "bg1.png";
     }
     else{
